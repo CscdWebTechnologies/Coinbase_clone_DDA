8acf58ccf03149571d38f83050445a508c38a347
Daniel A-Sereboo <dsereboo@gmail.com>
2021-05-11 19:15:26 +0000

    Section6 updated with styles

--- src/components/Section6.jsx
+++ src/components/Section6.jsx
@@ -6,16 +6,16 @@ const Section6=()=>{
       <Container fluid className="custom-background">
         <Row>
           <Col md={{ span: 3, offset:1 }} >
-            <h1 className="text-center">$335 Billion</h1>
-            <p className="text-center">Quarterly volume traded</p>
+            <h1 className="text-center header">$335B</h1>
+            <p className="text-center subtext">Quarterly volume traded</p>
           </Col>
           <Col  md={{ span: 3 }} >
-            <h1 className="text-center">100+</h1>
-            <p className="text-center">Quarterly volume traded</p>
+            <h1 className="text-center header">100+</h1>
+            <p className="text-center subtext">Quarterly volume traded</p>
           </Col>
           <Col  md={{ span: 3 }} >
-            <h1 className="text-center">56+M</h1>
-            <p className="text-center">Quarterly volume traded</p>
+            <h1 className="text-center header">56+M</h1>
+            <p className="text-center subtext">Quarterly volume traded</p>
           </Col>
         </Row>
        </Container>
