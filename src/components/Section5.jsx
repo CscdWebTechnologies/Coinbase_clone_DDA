16d9495781bc77ccb766f7cd2a05d32832c5b494
Torsu Derrick <torsuderrick2004@gmail.com>
2021-05-12 13:38:20 +0000

    Base Structure for Section 5 Completed

--- src/components/Section5.jsx
+++ src/components/Section5.jsx
@@ -1,11 +1,14 @@
 import React from "react"
+import {Container, Row,Col} from "react-bootstrap"
+import JumboHeader from "./JumboHeader"
 
 
-const Section6=()=>{
+const Section5=()=>{
   return(
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <Container>
+    <JumboHeader header="Create your cryptocurrency portfolio today" 
+     text="Coinbase has a variety of features that make it the best place to start trading"/>
+ </Container>
   )
 }
-export default  Section6    
+export default  Section5    
