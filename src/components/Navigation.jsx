a318938a6a7fccac6953d53236b6fe3dbfec5fd0
Araba Affran-Annan <aaffranannan@gmail.com>
2021-05-12 04:29:21 +0000

    Changed some styles of the navbar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,59 +1,63 @@
-import React from "react"
-import{Navbar, NavDropdown, Button, Nav} from "react-bootstrap"
-import logo from "./assets/logo.svg"
+import React from "react";
+import { Navbar, NavDropdown, Button, Nav } from "react-bootstrap";
+import logo from "./assets/logo.svg";
 
-const Navigation=()=>{
-    return (
-      <Navbar bg="light-custom" expand="lg">
-        <Navbar.Brand href="#home">
-          <img src={logo} alt="coinbase logo" width="112" height="20" />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Link href="#home">Item</Nav.Link>
-            <Nav.Link href="#home">Item</Nav.Link>
-            <Nav.Link href="#home">Item</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-              <Nav.Link className="pull">Sign In</Nav.Link>
-              <Nav.Link className="pull">
-                    <Button variant="success" size="sm" className="ml-5">Get Started</Button>
-              </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    );
-}
-export default Navigation
+const Navigation = () => {
+  return (
+    <Navbar bg="light-custom" expand="lg">
+      <Navbar.Brand href="#home">
+        <img src={logo} alt="coinbase logo" width="112" height="20" />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav" className="navbar-center">
+        <Nav className="navbar-center">
+          <Nav.Link href="#home">Prices</Nav.Link>
+          <Nav.Link href="#home">Learn</Nav.Link>
+          <Nav.Link href="#home">Individuals</Nav.Link>
+          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Businesses</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">
+              Another action
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">
+              Separated link
+            </NavDropdown.Item>
+          </NavDropdown>
+          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">
+              Another action
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">
+              Separated link
+            </NavDropdown.Item>
+          </NavDropdown>
+          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">
+              Another action
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">
+              Separated link
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        <Nav className="navbar-right">
+          <Nav.Link>Sign In</Nav.Link>
+          <Nav.Link>
+            <Button variant="primary" size="sm">
+              Get Started
+            </Button>
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+export default Navigation;
